1ee4e8aac0707e12abdd1234969270c931848fb5
huzaif <huzaif012@gmail.com>
2024-02-15 14:40:34 +0530

    Added put and Delete mapping in todo controller

--- src/main/java/com/todo/todo/controller/TodoController.java
+++ src/main/java/com/todo/todo/controller/TodoController.java
@@ -1,15 +1,14 @@
 package com.todo.todo.controller;
+
 import com.todo.todo.dto.TodoDto;
 import com.todo.todo.entity.Todo;
 import com.todo.todo.entity.User;
-import com.todo.todo.repository.TodoRepository;
 import com.todo.todo.service.TodoService;
 import com.todo.todo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -55,42 +54,69 @@ public class TodoController {
         if (user == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not found");
         }
-        System.out.println("user:"+username);
         // Fetch todos associated with the logged-in user
         List<Todo> todos = todoService.findByUser(user);
-        System.out.println("Todo size:"+todos.size());
 
 
         // Return the list of todos in the response
         return ResponseEntity.ok(todos);
     }
-//    @GetMapping
-//    public ResponseEntity<List<Todo>> getAllTodos() {
-//        List<Todo> todos = todoService.getAllTodos();
-//        return new ResponseEntity<>(todos, HttpStatus.OK);
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<Todo> createTodo(@RequestBody Todo todo) {
-//        Todo createdTodo = todoService.createOrUpdateTodo(todo);
-//        return new ResponseEntity<>(createdTodo, HttpStatus.CREATED);
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<Todo> getTodoById(@PathVariable Long id) {
-//        Todo todo = todoService.getTodoById(id);
-//        return new ResponseEntity<>(todo, HttpStatus.OK);
-//    }
-//    @PutMapping("/{id}")
-//    public ResponseEntity<Todo> updateTodo(@PathVariable Long id, @RequestBody Todo todo) {
-//        todo.setId(id);
-//        Todo updatedTodo = todoService.createOrUpdateTodo(todo);
-//        return new ResponseEntity<>(updatedTodo, HttpStatus.OK);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Void> deleteTodo(@PathVariable Long id) {
-//        todoService.deleteTodoById(id);
-//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//    }
+
+    @PutMapping("/user/{id}")
+    public ResponseEntity<?> updateTodoForLoggedInUser(Authentication authentication,@PathVariable Long id, @RequestBody TodoDto todoDto){
+        //Get the username of the logged in user from authentication
+        String username = authentication.getName();
+
+        //Fetch user details from the database based on the username
+        User user = userService.getUserByUsername(username);
+        if(user == null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not found");
+        }
+
+        //Fetching the todo
+        Todo todo = todoService.getTodoById(id);
+        if(todo == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No task found");
+        }
+        //checking if todo task belong to the logged in user
+        if(!todo.getUser().equals(user)){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Not authorize to update the todo");
+        }
+
+        //update the existing todo task
+        todo.setTitle(todoDto.getTitle());
+        todo.setCompleted(todoDto.isCompleted());
+
+        //Save the updated todo
+        todoService.createOrUpdateTodo(todo);
+
+        return ResponseEntity.ok("Todo updated successfully");
+    }
+    @DeleteMapping("/user/{id}")
+    public  ResponseEntity<?> DeletTodoForloggedInUser(Authentication authentication,@PathVariable Long id){
+        //Get the username of the logged- in user from authentication
+        String username = authentication.getName();
+
+        //Fetch user details from the database based on the username
+        User user = userService.getUserByUsername(username);
+        if(user == null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not found");
+        }
+
+        // Fetch the existing todo task
+        Todo existingTodo = todoService.getTodoById(id);
+        if (existingTodo == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Todo task not found");
+        }
+
+        // Check if the todo task belongs to the logged-in user
+        if (!existingTodo.getUser().equals(user)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You are not authorized to delete this todo task");
+        }
+
+        //deleting todo
+        todoService.deleteTodoById(id);
+        return ResponseEntity.ok("Deleted todo successfully");
+    }
+
 }
